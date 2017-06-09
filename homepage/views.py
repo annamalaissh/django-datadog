ee0e9344aa49e8af7c2c56662453502c16cc4a30
annamalaissh <annamalai.palaniappan@socialsweethearts.de>
2017-06-09 11:48:02 +0200

    Added homepage app

---
+++ homepage/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index_page(request, *args, **kwargs):
+    response = HttpResponse(content='ALL OK')
+    return response
